87581237e528f325f1ce502fa47585c5bf5d0ca4
samuel <proftoby97@gmail.com>
2023-06-05 08:57:58 +0100

    updated prod build

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const Blog_1 = __importDefault(require("./Routers/Blog"));
+const connect_1 = __importDefault(require("./DB/connect"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({ origin: true }));
+connect_1.default;
+app.use(express_1.default.json()); //allows parsing incoming request to json
+app.use(Blog_1.default);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('listening on port ' + port);
+});
